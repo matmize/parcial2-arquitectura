a7003acfa5b4df28680bdea95abaed706cd91688
matmize <mateomontoya5@gmail.com>
2022-10-04 07:50:59 -0500

    Update Buy.java

--- src/main/java/co/edu/unisabana/parcialarquitectura/service/Buy.java
+++ src/main/java/co/edu/unisabana/parcialarquitectura/service/Buy.java
@@ -3,19 +3,24 @@ package co.edu.unisabana.parcialarquitectura.service;
 
 import co.edu.unisabana.parcialarquitectura.repository.Database;
 
+import javax.xml.crypto.Data;
+
 public class Buy {
 
 
   public String makePurchase(int vendorCode, int buyerCode, String item) {
-    Database database = new Database();
-    if (buyerCode == vendorCode) {
-      throw new IllegalSaleException(vendorCode, buyerCode);
+
+    public void ventaProducto(Database database) {
+      Database database = new Database();
+      if (buyerCode == vendorCode) {
+        throw new IllegalSaleException(vendorCode, buyerCode);
+      }
+      int result = database.savePurchase(buyerCode, item);
+      if (result == 1) {
+        return "Product sold";
+      }
+      return "Sorry :( the sale was canceled in the last minute, try again";
     }
-    int result = database.savePurchase(buyerCode, item);
-    if (result == 1) {
-      return "Product sold";
-    } else {
-      return "The sale was not possible";
-    }
+
   }
 }
